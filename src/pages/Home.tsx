4ce7659bb987ce85ec5a802ed9acac3d64d65117
hphothong <hphothong@gmail.com>
2020-12-22 16:35:12 -0700

    Add hash router for github pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Home: React.FC = () => <div>Home Screen</div>;
+
+export default Home;
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Projects: React.FC = () => <div>Projects Screen</div>;
+
+export default Projects;
