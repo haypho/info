ffe27e720ce8819878dc23b4e51367e523a51bfb
hphothong <hphothong@gmail.com>
2020-12-26 14:52:04 -0700

    Add default text to website

--- src/pages/CodingChallenges.tsx
+++ src/pages/CodingChallenges.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 const CodingChallenges: React.FC = () => (
-  <h1>
-    Still working through this one! Check out my Github for the current progress
-  </h1>
+  <>
+    <h1>Sorry!</h1>
+    <strong>
+      Still working through this one! Check out my Github for the current
+      progress
+    </strong>
+  </>
 );
 
 export default CodingChallenges;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,71 +2,15 @@ import React from "react";
 
 const Home: React.FC = () => (
   <div>
-    <p>{paragraph}</p>
-    {/* <p>{paragraph}</p> */}
+    <h1>Welcome!</h1>
+    <p>
+      I would first like to thank you for viewing my portfolio! It does not look
+      like much now, but the plan is to continue to add personal projects and
+      coding challenges in the near future. People always say that time is
+      valuable, but seasoned developers know that documentation is too! 😉
+    </p>
+    <strong>More coming soon!</strong>
   </div>
 );
 
 export default Home;
-
-const paragraph = `
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Mauris augue neque
-      gravida in fermentum et sollicitudin. Venenatis lectus magna fringilla
-      urna porttitor rhoncus. Massa sed elementum tempus egestas sed. Risus in
-      hendrerit gravida rutrum. Elementum sagittis vitae et leo. Risus commodo
-      viverra maecenas accumsan lacus vel facilisis. Sagittis eu volutpat odio
-      facilisis mauris sit. Vitae nunc sed velit dignissim sodales ut eu sem
-      integer. Vitae congue eu consequat ac felis donec et odio. Ultrices in
-      iaculis nunc sed augue lacus. Interdum varius sit amet mattis. Luctus
-      venenatis lectus magna fringilla urna porttitor rhoncus dolor purus. Nunc
-      eget lorem dolor sed. Facilisi etiam dignissim diam quis enim lobortis
-      scelerisque fermentum dui. Scelerisque purus semper eget duis at tellus at
-      urna condimentum. Diam phasellus vestibulum lorem sed risus. Sed arcu non
-      odio euismod lacinia at quis. Sed arcu non odio euismod. Eget felis eget
-      nunc lobortis mattis aliquam faucibus purus. Cursus metus aliquam eleifend
-      mi in nulla posuere. Non curabitur gravida arcu ac tortor dignissim
-      convallis aenean et. Arcu risus quis varius quam quisque id. Vitae
-      elementum curabitur vitae nunc. Pellentesque dignissim enim sit amet
-      venenatis urna cursus eget nunc. Pretium fusce id velit ut tortor pretium
-      viverra. Adipiscing vitae proin sagittis nisl rhoncus. Quisque sagittis
-      purus sit amet volutpat. Vitae congue mauris rhoncus aenean vel. Bibendum
-      neque egestas congue quisque egestas. Tincidunt arcu non sodales neque
-      sodales ut etiam sit. Felis donec et odio pellentesque diam volutpat
-      commodo sed egestas. Aenean et tortor at risus viverra adipiscing.
-      Ultrices eros in cursus turpis massa. Nisl purus in mollis nunc sed.
-      Fermentum posuere urna nec tincidunt praesent semper feugiat. Vitae proin
-      sagittis nisl rhoncus mattis. Eu consequat ac felis donec et odio
-      pellentesque diam. Eget gravida cum sociis natoque penatibus. Ut tristique
-      et egestas quis. Quis imperdiet massa tincidunt nunc. Faucibus purus in
-      massa tempor nec feugiat nisl pretium. Volutpat blandit aliquam etiam erat
-      velit. Est placerat in egestas erat imperdiet sed euismod nisi porta. Nibh
-      tellus molestie nunc non blandit massa enim. Platea dictumst vestibulum
-      rhoncus est. Malesuada nunc vel risus commodo. Ut aliquam purus sit amet
-      luctus venenatis lectus. Odio eu feugiat pretium nibh. Tristique
-      sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Ipsum
-      faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Magna sit
-      amet purus gravida quis blandit turpis cursus. Pellentesque elit
-      ullamcorper dignissim cras tincidunt lobortis. Diam volutpat commodo sed
-      egestas egestas. Bibendum at varius vel pharetra vel turpis. Nunc congue
-      nisi vitae suscipit tellus mauris a diam maecenas. Cursus metus aliquam
-      eleifend mi in nulla. Non blandit massa enim nec dui nunc. Tellus integer
-      feugiat scelerisque varius. Aliquam malesuada bibendum arcu vitae
-      elementum. Fusce ut placerat orci nulla pellentesque. Nullam ac tortor
-      vitae purus faucibus ornare suspendisse. Nunc faucibus a pellentesque sit
-      amet. Leo vel fringilla est ullamcorper eget nulla facilisi. Nunc
-      scelerisque viverra mauris in aliquam sem fringilla ut. Massa tincidunt
-      dui ut ornare lectus sit amet est placerat. Scelerisque eu ultrices vitae
-      auctor eu augue ut lectus arcu. Donec pretium vulputate sapien nec
-      sagittis aliquam malesuada bibendum arcu. Sed adipiscing diam donec
-      adipiscing tristique risus. Tincidunt lobortis feugiat vivamus at augue
-      eget arcu dictum. Turpis egestas pretium aenean pharetra. Feugiat nibh sed
-      pulvinar proin. Nisl nisi scelerisque eu ultrices vitae. Purus sit amet
-      volutpat conse
-      suscipit adipiscing bibendum est. Magna etiam tempor orci eu lobortis.
-      Blandit turpis cursus in hac habitasse. Suspendisse interdum consectetur
-      libero id. Vitae congue eu consequat ac felis donec. Quam id leo in vitae
-      turpis massa. Ipsum suspendisse ultrices gravida dictum fusce. Leo duis ut
-      diam quam nulla porttitor massa id neque. Risus at ultrices mi tempus.
-      Donec enim diam vulputate ut pharetra sit amet. Tristique senectus et
-  `;
