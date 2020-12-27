8d6deb467badeb1b3270736b740e3e9bcca931c2
hphothong <hphothong@gmail.com>
2020-12-27 12:48:50 -0700

    Add husky and lint-staged

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
 const path = require('path');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
