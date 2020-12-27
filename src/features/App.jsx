3a26e3788a3707003118f3d7b9e22d893d1c0375
hphothong <hphothong@gmail.com>
2020-12-27 12:29:52 -0700

    Add initial React setup with webpack/babel

---
+++ src/features/App.jsx
@@ -0,0 +1,6 @@
+import './App.css';
+import React from 'react';
+
+const App = () => <div>Hello from main App. This is an addition.</div>;
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'static/js/[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, 'public/index.html'),
+    }),
+  ],
+};
