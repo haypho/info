6ca507dafe0bc76f8f4006439ea9b3504df857e3
Hayden Phothong <hphothong@gmail.com>
2021-03-21 18:36:33 -0600

    Add profile image to the main content

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -4,14 +4,12 @@ import React from 'react';
 const App: React.FC = () => (
   <>
     <header className="app-header">
-      <div className="container flex flex-col">
-        <div className="app-title-area">
-          <h2 className="app-title">Hayden Phothong</h2>
-          <p className="app-subtitle">PERSONAL PORTFOLIO</p>
-        </div>
+      <div className="app-title-area">
+        <h2 className="app-title">Hayden Phothong</h2>
+        <p className="app-subtitle">PERSONAL PORTFOLIO</p>
       </div>
     </header>
-    <div className="app-content container" />
+    <img src="/assets/images/profile.jpg" alt="profile" />
     <footer className="app-footer">
       <p>Hayden Phothong &copy; 2020</p>
     </footer>
