475b4061c3780cb3da44176b04cbc4290097ed88
Hayden Phothong <hphothong@gmail.com>
2021-05-02 12:13:59 -0600

    Add fallback property for avatar

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,5 +1,5 @@
 import './Avatar.scss';
-import React, { FC } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 
 export interface AvatarProps {
   src: string;
@@ -7,20 +7,30 @@ export interface AvatarProps {
   title?: string;
   subtitle?: string;
   className?: string;
+  fallback?: string;
 }
 
 const Avatar: FC<AvatarProps> = ({
-  src, alt, title, subtitle, className,
-}: AvatarProps) => (
-  <div className={['avatar', className].join(' ')}>
-    <img className="avatar-img" src={src} alt={alt} />
-    {title || subtitle ? (
-      <div className="avatar-details">
-        {title && (<p className="avatar-title">{title}</p>)}
-        {subtitle && (<p className="avatar-subtitle">{subtitle}</p>)}
-      </div>
-    ) : null}
-  </div>
-);
+  src, alt, title, subtitle, className, fallback,
+}: AvatarProps) => {
+  const [source, setSource] = useState<string>(src);
+  const onErrorLoadingImage = useCallback(() => {
+    if (fallback) {
+      setSource(fallback);
+    }
+  }, [fallback, setSource]);
+
+  return (
+    <div className={['avatar', className].join(' ')}>
+      <img className="avatar-img" src={source} alt={alt} onError={onErrorLoadingImage} />
+      {title || subtitle ? (
+        <div className="avatar-details">
+          {title && (<p className="avatar-title">{title}</p>)}
+          {subtitle && (<p className="avatar-subtitle">{subtitle}</p>)}
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
 export default Avatar;
