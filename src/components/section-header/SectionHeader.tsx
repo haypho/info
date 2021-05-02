782fc6dfdce4a1c67ece413be9a25718742c40be
Hayden Phothong <hphothong@gmail.com>
2021-05-02 13:17:59 -0600

    Add iconLeft property to section header

--- src/components/section-header/SectionHeader.tsx
+++ src/components/section-header/SectionHeader.tsx
@@ -1,12 +1,16 @@
 import './SectionHeader.scss';
 import React, { FC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 export interface SectionHeaderProps {
   title: string;
+  iconLeft?: IconProp;
 }
 
-const SectionHeader: FC<SectionHeaderProps> = ({ title }: SectionHeaderProps) => (
+const SectionHeader: FC<SectionHeaderProps> = ({ title, iconLeft }: SectionHeaderProps) => (
   <div className="section-header">
+    {iconLeft && <FontAwesomeIcon className="section-header-icon-left" icon={iconLeft} />}
     <p className="section-header-title">{title}</p>
   </div>
 );
