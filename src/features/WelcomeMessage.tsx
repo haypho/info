51ac9b171d9991b006c50af425d6347ef899ccab
Hayden Phothong <hphothong@gmail.com>
2021-05-03 08:31:41 -0600

    Reword the welcom message verbiage

--- src/features/WelcomeMessage.tsx
+++ src/features/WelcomeMessage.tsx
@@ -9,16 +9,11 @@ const WelcomeMessage: FC = () => (
   <Container>
     <SectionHeader title="Welcome!" iconLeft={faBookOpen} />
     <p>
-      Thank you for taking the time to look at my personal portfolio!
-      I have a few of my projects listed below! I try to spend most of my time
-      working on these projects. I have found that working on them is
-      the easiest way to learn! Natural languages, programming
-      languages, frameworks, tools, new skills, you name it! I want to know it all!
-    </p>
-    <p>
-      Eventually, I would like to specialize in a particular area in Software Engineering.
-      Right now, I am reaching out to every area to get a feel for the spot where I shine brightest!
-      If you would like to see my work history, check out my
+      Thank you for taking the time to look at my personal portfolio. Below I have listed
+      a few of my projects. These projects are the ones that taught me the most. I was able
+      to deep-dive into front-end frameworks, the JavaScript/TypeScript ecosystem,
+      cloud solutions for data storage/retrieval, as well as the publishing process for the
+      App Store and Play Store. If you would like to see my work history, check out my
       {' '}
       <a href={Resume} target="_blank" rel="noreferrer">résumé</a>
       {' '}
