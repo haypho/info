94ac0d70a756e667cd083a461b447e67f166dfdd
Hayden Phothong <hphothong@gmail.com>
2021-05-02 18:28:41 -0600

    Fix personal LinkedIn URL

--- src/constants/personal.constants.ts
+++ src/constants/personal.constants.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export const personalEmail = 'hphothong@gmail.com';
 export const personalEmailURL = `mailto:${personalEmail}`;
-export const personalLinkedInURL = 'https://www.linkedin.com/in/hayden-phothong-a2393312';
+export const personalLinkedInURL = 'https://www.linkedin.com/in/hayden-phothong-a23933129';
 export const personalGithubURL = 'https://www.github.com/hphothong';
