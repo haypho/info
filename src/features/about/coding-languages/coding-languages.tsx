79eab220e3f1b8523919913b5986d17dafa8231b
Hayden Phothong <hphothong@gmail.com>
2025-04-01 22:48:31 -0700

    fix(features): typo in coding languages (i.e., yeo -> YoE)

--- src/features/about/coding-languages/coding-languages.tsx
+++ src/features/about/coding-languages/coding-languages.tsx
@@ -1,15 +1,21 @@
-import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { useMemo } from "react";
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 
 const CODING_LANGUAGES_DATA = [
-  { id: "TypeScript", yeo: 7, fill: "var(--chart-1)" },
-  { id: "JavaScript", yeo: 7, fill: "var(--chart-1)" },
-  { id: "HTML", yeo: 7, fill: "var(--chart-1)" },
-  { id: "CSS", yeo: 7, fill: "var(--chart-1)" },
-  { id: "Java", yeo: 5, fill: "var(--chart-2)" },
-  { id: "C#", yeo: 3, fill: "var(--chart-3)" },
-  { id: "Python", yeo: 2, fill: "var(--chart-4)" },
-];
+  { id: "TypeScript", YoE: 7, fill: "var(--chart-1)" },
+  { id: "JavaScript", YoE: 7, fill: "var(--chart-2)" },
+  { id: "HTML", YoE: 7, fill: "var(--chart-3)" },
+  { id: "CSS", YoE: 7, fill: "var(--chart-4)" },
+  { id: "Java", YoE: 5, fill: "var(--chart-5)" },
+  { id: "C#", YoE: 3, fill: "var(--chart-1)" },
+  { id: "Python", YoE: 2, fill: "var(--chart-2)" },
+].sort((a, b) => a.YoE - b.YoE || a.id.localeCompare(b.id));
 
 const CHART_CONFIG = {} satisfies ChartConfig;
 
@@ -18,21 +24,22 @@ export function CodingLanguages() {
     <ChartContainer config={CHART_CONFIG} className="w-full min-h-52">
       <BarChart
         accessibilityLayer
-        data={CODING_LANGUAGES_DATA.sort((a, b) => a.yeo - b.yeo)}
+        data={CODING_LANGUAGES_DATA}
         layout="vertical"
       >
-        <CartesianGrid />
+        <CartesianGrid horizontal={false} />
+        <ChartTooltip content={<ChartTooltipContent indicator="line" />} />
         <XAxis
           type="number"
-          dataKey="yeo"
+          dataKey="YoE"
           label={{
             value: "Years of Experience (YoE)",
             position: "insideBottom",
-            offset: -5,
+            offset: -2,
           }}
         />
         <YAxis type="category" dataKey="id" />
-        <Bar dataKey="yeo" />
+        <Bar dataKey="YoE" />
       </BarChart>
     </ChartContainer>
   );
